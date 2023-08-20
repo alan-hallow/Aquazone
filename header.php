4a9094afefebf14ea9f504e42152a490937a473f
Alan Johnson <142657630+alan-hallow@users.noreply.github.com>
2023-08-20 19:27:26 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,76 @@
+<?php
+    session_start();
+
+?>
+
+
+<!-- <!DOCTYPE html>
+<html>
+    <head>
+        <title>header</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/headercss.css">
+
+        <script src="https://kit.fontawesome.com/89589cc083.js" crossorigin="anonymous"></script>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@600&display=swap" rel="stylesheet">
+
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@700&display=swap" rel="stylesheet">
+
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Makasar&display=swap" rel="stylesheet">
+
+    </head>
+    <body> -->
+
+        <div id="myNav" class="overlay">
+            <button class="btnclose" onclick="closeNav()"><i class="fa-solid fa-xmark"></i></button>
+    
+            <div class="overlay-content">
+                <a href="#" class="links">Home</a>
+                <a href="productsshow.php" class="links">Products</a>
+                <a href="#" class="links">Water Supply</a>
+                <a href="#" class="links">Borewell</a>
+                <a href="#" class="links">Service Men</a>
+                <a href="#" class="links">About</a>
+            </div>
+
+            <div class="account">
+                <?php
+                    if(isset($_SESSION["username"])){
+                        echo "<a href='profile.php' class='login'><i class='fa-solid fa-user'></i></a>";
+                        echo "<a href='includes/logout-inc.php' class='login'><i class='fa-solid fa-right-from-bracket'></i></a>";
+                    }
+                    else{
+                        echo "<a href='login.php' class='login'>login</a>";
+                        echo "<a href='signup.php' class='login'>signup</a>";
+                    }
+                ?>
+
+            </div>
+            <div id="menu-background-pattern"></div>
+
+
+
+
+
+        </div>
+        <div class="contents">
+        
+            <div class="header">
+                <div class="aquazone-logo">
+                    <font color="#007aff">Aqua</font>
+                    <font color="#ffffff">Zone</font>
+                </div>
+                <div class="pagename">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                </div>
+                <button class="btnopen" onclick="openNav()"><i class="fa-solid fa-bars-staggered" ></i></button>
+            </div>
+        </div>
+
+        <script src="js/headerjs.js"></script>
+<!--         
+    </body>
+</html> -->
---
+++ login.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>AquaZone</title>
+<link rel="stylesheet" href="css/logincss.css">
+
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Anton&family=IBM+Plex+Sans:wght@700&family=Nunito&family=Oswald:wght@500&display=swap" rel="stylesheet">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow&display=swap" rel="stylesheet">
+
+
+</head>
+
+
+<body>
+
+<div id="login-form-wrap">
+
+
+  <h2>
+    <font color="#007aff">Aqua</font-color>
+    <font color="#000000">Zone</font-color>
+  </h2>
+  <form id="login-form" class="form" action="includes/login-inc.php" method="post">
+    <p>
+      <label>Username</label>
+      <input type="text" id="username" name="user_name" placeholder="Username/email" ><i class="validation"><span></span><span></span></i>
+      </p>
+      <p>
+      <label>Password</label>
+      <input type="password" id="mypassword" name="password"  placeholder="Password" ><i class="validation"><span></span><span></span></i>
+      </p>
+      <p>
+      <button type="submit" name="submit" id="login" class="submitbutton">Login</button>
+      </p>
+  </form>
+
+
+  <div id="create-account-wrap">
+   <p>Not a member? <a style="text-decoration:none" href="signup.php">Create Account</a><p>
+  </div>
+
+
+</div>
+<?php 
+
+if (isset($_GET["error"])){
+    if ($_GET["error"] == "emptyinput"){
+
+        echo "<p class='errors'>Fill in all fields</p>";
+
+    }
+    elseif($_GET["error"] == "wronglogin"){
+
+        echo "<p class='errors'>Incorrect login information</p>";
+
+    }
+}
+
+
+?>
+
+</body>
+
+</html>
+
